c76935211198dcb49a241ec8e7f5db2f8fe32054
sarvagya-singh-pro <68740017+sarvagya230@users.noreply.github.com>
2023-08-10 00:47:27 +0530

    collapsables

--- index.js
+++ index.js
@@ -15,15 +15,46 @@ barba.init({
   });
 
   const states=[0,0,0]
+  document.querySelector('.menu').addEventListener('click',()=>{
+    alert("click")
+  })
+  document.querySelectorAll('.collaspable').forEach(el=>{
+    el.style.display="none"
+  });
   document.querySelectorAll('.regulist div').forEach((el,index)=>{
     el.addEventListener("click",(ev)=>{
      if(!states[index]){
       states[index]=1
       ev.target.children[0].classList.add("active");
+      ev.target.children[0].classList.remove("unactive")
+      console.log(index)
+
+      if(index==2){
+      document.querySelectorAll('.collaspable')[index-1].style.display="block";
+      }
+      else if(index==4){
+        
+      document.querySelectorAll('.collaspable')[index-2].style.display="block";
+      }
+      else{
+        document.querySelectorAll('.collaspable')[index].style.display="block";
+      }
+     
      }
      else{
       states[index]=0;
       ev.target.children[0].classList.add("unactive");
+      ev.target.children[0].classList.remove("active")
+      if(index==2){
+        document.querySelectorAll('.collaspable')[index-1].style.display="none";
+        }
+        else if(index==4){
+          
+        document.querySelectorAll('.collaspable')[index-2].style.display="none";
+        }
+        else{
+          document.querySelectorAll('.collaspable')[index].style.display="none";
+        }
      }
     })
   })
