4c9bbb1e7b20e37a3eb87787945da3fcc89a1ef9
sarvagya-singh-pro <68740017+sarvagya230@users.noreply.github.com>
2023-08-12 17:14:45 +0530

    about us debugging

--- about.js
+++ about.js
@@ -1,9 +1,12 @@
 console.clear();
-/* The encoding is super important here to enable frame-by-frame scrubbing. */
-
-// ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
-// ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output_960.mp4
-
+window.addEventListener("scroll", function(){
+    if(window.scrollY!=0){
+    document.querySelector('.From').style.opacity=0;
+    }
+    else{
+        document.querySelector('.From').style.opacity=1;
+    }
+})
 const video = document.querySelector("video");
 let src = video.currentSrc || video.src;
 console.log(video, src);
